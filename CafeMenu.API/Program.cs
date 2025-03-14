d5ad012b9da3e4dbf89f1dd984e3564365985636
Ali Barzegar <92709989+ALB1381@users.noreply.github.com>
2025-03-14 08:58:21 -0700

    Update Program.cs

--- CafeMenu.API/Program.cs
+++ CafeMenu.API/Program.cs
@@ -6,10 +6,8 @@ using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<CafeContext>(options =>
 {
@@ -27,14 +25,11 @@ builder.Services.AddCors(options =>
     });
 });
 
-//options.SuppressConsumesConstraintForFormFileParameters = true; to not accept any file in hole of project
-// options.SuppressInferBindingSourcesForParameters = true; The parameters are not going to be accepted by defult and you should knowen theme by atribute\
-//  options.SuppressMapClientErrors = true; it will disable the problems response defult such as Ok();
-// This part of code will start to log 400 Reses
+
 builder.Services.AddControllers()
     .ConfigureApiBehaviorOptions(options =>
     {
-        // To preserve the default behavior, capture the original delegate to call later.
+    
         var builtInFactory = options.InvalidModelStateResponseFactory;
 
         options.InvalidModelStateResponseFactory = context =>
@@ -42,21 +37,14 @@ builder.Services.AddControllers()
             var logger = context.HttpContext.RequestServices
                                 .GetRequiredService<ILogger<Program>>();
 
-            // Perform logging here.
-            // ...
-
-            // Invoke the default behavior, which produces a ValidationProblemDetails
-            // response.
-            // To produce a custom response, return a different implementation of 
-            // IActionResult instead.
             return builtInFactory(context);
         };
     });
-// other service configurations
+
 var app = builder.Build();
 
 app.UseCors("CorsPolicy");
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
